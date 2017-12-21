b633826e32a94e6d3a6523298fc7e1677171b98d
Norman Coloma García <ua.norman@gmail.com>
2017-12-21 21:15:14 +0100

    Dispatching actions directly from playlist container component

--- src/containers/playlist/playlist.js
+++ src/containers/playlist/playlist.js
@@ -1,31 +1,59 @@
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 //Actions creators
-import { toggleFilm, addToWatchlist, removeFromWatchlist } from '../../actions/index';
+import { toggleFilm, addToWatchlist, removeFromWatchlist, fetchFilm } from '../../actions/index';
 
-//Reducers selectors 
+//Reducers and actions
 import { getPlaylist } from '../../reducers'
+
 //Components
 import FilmList from '../../components/film-list/film-list';
+
+class PlaylistComponent extends Component {
+    componentDidMount() {
+        this.fetchFilms();
+    }
+
+    render() {
+        const { addToWatchlist, removeFromWatchlist, ...rest } = this.props;
+        return <FilmList 
+            {...rest}
+            onAddToWatchlist={addToWatchlist}
+            onRemoveFromWatchlist={removeFromWatchlist}
+         />
+    }
+
+    fetchFilms() {
+        const { fetchFilm } = this.props;
+        fetchFilm('Shutter Island');
+        fetchFilm('Django Unchained');
+        fetchFilm('Coco');
+        fetchFilm('Star Wars: Episode III');
+    }
+}
 
 const mapStateToProps = (state) => ({
     playlist: getPlaylist(state)
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    onAddToWatchlist: film => {
+    addToWatchlist: film => {
         dispatch(toggleFilm(film.id)),
         dispatch(addToWatchlist(film))
     },
-    onRemoveFromWatchlist: film => {
+    removeFromWatchlist: film => {
         dispatch(toggleFilm(film.id)),
         dispatch(removeFromWatchlist(film.id))
+    },
+    fetchFilm: title => {
+        dispatch(fetchFilm(title))
     }
 });
 
 const Playlist = withRouter(connect(
     mapStateToProps,
     mapDispatchToProps
-)(FilmList));
+)(PlaylistComponent));
 
 export default Playlist;
