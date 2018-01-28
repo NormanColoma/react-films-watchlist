7dbf1a3d681bbcce6e939e21476eddee1d175e9b
Norman Coloma García <ua.norman@gmail.com>
2018-01-28 12:14:07 +0100

    Added new styles to film list view

--- src/components/film-list/film-detail/film-detail.js
+++ src/components/film-list/film-detail/film-detail.js
@@ -1,24 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './film-detail.css';
 
 const FilmDetail = ({ film, onFavFilm }) => {
     const favClass = film.inWatchList ? 'film fav' : 'film rating';
+    const linkToDetails = `/films/${film.id}`;
+
     return(
         <div className="film container">
+            <img src={film.poster} alt={film.name} />
             <div className="film titleContainer">
                 <h2 className="film title">{film.name}, {film.releasedDate}</h2>
-                <div className="film extra-details-container">
-                    <p className="film extra-details">{film.director}</p>
-                    <span className="film divider"> | </span> 
-                    <p className="film extra-details">{film.genre}</p>
-                    <div className={favClass} onClick={() => onFavFilm(film)}>
-                        <i className="fab fa-gratipay"></i>
-                    </div>
+                <div className={favClass} onClick={() => onFavFilm(film)}>
+                    <i className="fas fa-heart"></i>
                 </div>
-                
             </div>
-            <img src={film.poster} alt={film.name} />
             <p className="synopsys">{film.synopsys}</p>
+            <div className="film extra-details-container">
+                <div className="film extra-details-left-container">
+                    <ul>
+                        <li>
+                            <i className="fas fa-video"></i><p className="film extra-details">{film.director}</p>
+                        </li>
+                        <li>
+                            <i className="fas fa-tag genre"></i><p className="film extra-details">{film.genre}</p>
+                        </li>
+                    </ul>
+                </div>
+                <Link to={linkToDetails} className="default-btn">See More</Link>
+            </div>
         </div>
     );
 }
--- src/components/film-list/film-list.js
+++ src/components/film-list/film-list.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import FilmDetail from './film-detail/film-detail';
 import Film from '../../containers/film/film';
 import FilmFilter from './film-filter/film-filter';
@@ -8,11 +8,9 @@ import './film-list.css';
 const FilmList = ({ playlist, onAddToWatchlist, onRemoveFromWatchlist, onFilterPlaylist, filter, match }) => {
     const films = playlist.map((film) => {
         const onFavFilm = film.inWatchList ? onRemoveFromWatchlist : onAddToWatchlist;
-        const linkToDetails = `/films/${film.id}`;
 
         return <li key={film.id}>
             <FilmDetail film={film} onFavFilm={onFavFilm} />
-            <Link to={linkToDetails} className="default-btn">See More</Link>
         </li>;
     });
 
