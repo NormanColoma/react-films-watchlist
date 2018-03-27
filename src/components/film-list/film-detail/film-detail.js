95791071786bbeb194ba9490aaf0adf35e5f8fd1
Norman Coloma García <ua.norman@gmail.com>
2018-03-27 23:51:54 +0200

    Adapted film-container to flexbox

--- src/components/film-list/film-detail/film-detail.js
+++ src/components/film-list/film-detail/film-detail.js
@@ -14,19 +14,25 @@ const FilmDetail = ({ film, onFavFilm }: Props) => {
     const favClass = film.inWatchList ? 'film fav' : 'film rating';
     const linkToDetails = `/films/${film.id}`;
 
-    return(
+    return (
         <div className="film container">
-            <img src={film.poster} alt={film.name} />
-            <div className="film titleContainer">
-                <div className="film titleReleaseContainer">
-                    <h2 className="film title">{film.name}</h2>
-                    <span><i className="fas fa-film"></i> {film.releasedDate}</span>
+            <div className="film-inner-container">
+                <img src={film.poster} alt={film.name} />
+                <div className="film titleContainer">
+                    <div className="film titleReleaseContainer">
+                        <div className="film-container__title-release">
+                            <h2 className="film title">{film.name}</h2>
+                            <span><i className="fas fa-film"></i> {film.releasedDate}</span>
+                        </div>
+                        <div className={favClass} onClick={() => onFavFilm(film)}>
+                            <i className="fas fa-heart"></i>
+                        </div>
+                    </div>
+                    <p className="synopsys">{film.synopsys}</p>
                 </div>
-                <div className={favClass} onClick={() => onFavFilm(film)}>
-                    <i className="fas fa-heart"></i>
+                <div className="film-container__synopsys">
                 </div>
             </div>
-            <p className="synopsys">{film.synopsys}</p>
             <div className="film extra-details-container">
                 <div className="film extra-details-left-container">
                     <ul>
@@ -38,7 +44,9 @@ const FilmDetail = ({ film, onFavFilm }: Props) => {
                         </li>
                     </ul>
                 </div>
-                <Link to={linkToDetails} className="default-btn">See More</Link>
+                <div className="film__link-button">
+                    <Link to={linkToDetails} className="default-btn">See More</Link>
+                </div>
             </div>
         </div>
     );
