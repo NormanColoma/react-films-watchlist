cbb99104e769e39ce193fc9c4a5819549e18495d
Norman Coloma García <ua.norman@gmail.com>
2018-04-03 16:42:55 +0200

    Creating layout for film reviews

---
+++ src/components/film/film-reviews/film-reviews.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './film-reviews.css';
+
+const FilmReviews = () => {
+    return (
+        <div className="reviews-container">
+            <h1>Reviews</h1>
+            <div className="review-container">
+                <div className="review-container__header">
+                    <h3 className="review-container__header">Paco</h3>
+                    <span>3 April 2018</span>
+                </div>
+                <p className="review-container__text"><q>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido
+                     el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona
+                     que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró
+                      hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto
+                      de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en
+                      los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más
+                      recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones
+                      de Lorem Ipsum.</q>
+                </p>
+            </div>
+            <div className="review-container">
+                <div className="review-container__header">
+                    <h3 className="review-container__header">Paco</h3>
+                    <span>3 April 2018</span>
+                </div>
+                <p className="review-container__text"><q>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido
+                     el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona
+                     que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró
+                      hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto
+                      de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en
+                      los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más
+                      recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones
+                      de Lorem Ipsum.</q>
+                </p>
+            </div>
+            <div className="review-container">
+                <div className="review-container__header">
+                    <h3 className="review-container__header">Paco</h3>
+                    <span>3 April 2018</span>
+                </div>
+                <p className="review-container__text"><q>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido
+                     el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona
+                     que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró
+                      hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto
+                      de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en
+                      los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más
+                      recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones
+                      de Lorem Ipsum.</q>
+                </p>
+            </div>
+            <div className="review-container">
+                <div className="review-container__header">
+                    <h3 className="review-container__header">Paco</h3>
+                    <span>3 April 2018</span>
+                </div>
+                <p className="review-container__text"><q>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido
+                     el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona
+                     que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró
+                      hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto
+                      de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en
+                      los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más
+                      recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones
+                      de Lorem Ipsum.</q>
+                </p>
+            </div>
+        </div>
+    )
+};
+
+export default FilmReviews;
--- src/components/film/film.js
+++ src/components/film/film.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Film from '../../domain/Film';
 import './film.css';
+import FilmReviews from './film-reviews/film-reviews';
 
 type Props = {
     film: Film,
@@ -27,34 +28,37 @@ const FilmView = ({ film, loading, onAddToWatchlist, onRemoveFromWatchlist }: Pr
     }
     return (
         <div className="default-conatiner">
-            <div className="film-container__img">
-                <img className="default-img" src={film.poster} alt={film.name} />
-            </div>
-            <div className="film-container__options">
-                <div className="film-container__title">
-                    <h1 className="default-title">{film.name}<span>({film.releasedDate})</span></h1>
-                    <ul>
-                        <li>
-                            <i className="fas fa-star gold-color"></i>
-                            <span>{film.rating}</span>
-                        </li>
-                    </ul>
+            <div className="film-container__inner">
+                <div className="film-container__img">
+                    <img className="default-img" src={film.poster} alt={film.name} />
                 </div>
-                <div className="film-container__synopsys">
-                    <h3>Synopsys</h3>
-                    <p>{film.synopsys}</p>
+                <div className="film-container__options">
+                    <div className="film-container__title">
+                        <h1 className="default-title">{film.name}<span>({film.releasedDate})</span></h1>
+                        <ul>
+                            <li>
+                                <i className="fas fa-star gold-color"></i>
+                                <span>{film.rating}</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="film-container__synopsys">
+                        <h3>Synopsys</h3>
+                        <p>{film.synopsys}</p>
+                    </div>
+                    <div className="film-container__cast">
+                        <h3>Cast</h3>
+                        <p>{film.actors}</p>
+                    </div>
+                    <div className="film-container__cast">
+                        <h3>Director</h3>
+                        <p>{film.director}</p>
+                    </div>
+                    <div className="film-container__actions">
+                        {input}
+                    </div>
                 </div>
-                <div className="film-container__cast">
-                    <h3>Cast</h3>
-                    <p>{film.actors}</p>
-                </div>
-                <div className="film-container__cast">
-                    <h3>Director</h3>
-                    <p>{film.director}</p>
-                </div>
-                <div className="film-container__actions">
-                    {input}
-                </div>
+                <FilmReviews />
             </div>
         </div>
     )
