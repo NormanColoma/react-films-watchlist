9e70bcf272be09c6cd64ca1be9e52fbdf1766f8a
Norman Coloma García <ua.norman@gmail.com>
2018-04-04 18:30:22 +0200

    Adding button for display next suggested film

--- src/components/film/film-suggestions/film-suggestions-resume/film-suggestions-resume.js
+++ src/components/film/film-suggestions/film-suggestions-resume/film-suggestions-resume.js
@@ -2,19 +2,28 @@ import React from 'react';
 import './film-suggestions-resume.css';
 
 
-const FilmSuggestionsResume = ({ film }) => {
+const FilmSuggestionsResume = ({ film, onClickNext }) => {
     return (
         <div className="film-suggested__container">
             <div className="film-suggested__details">
                 <img src={film.poster} />
             </div>
             <div className="film-suggested__info">
-                <h4>
-                    {film.name}
-                    <span className="film-suggested__release">({film.releasedDate})</span>
-                </h4>
-                <p className="film-suggested__genres">{film.genre}</p>
+                <div className="film-suggested__info-header">
+                    <h4>
+                        {film.name}
+                        <span className="film-suggested__release">({film.releasedDate})</span>
+                    </h4>
+                    <p className="film-suggested__genres">{film.genre}</p>
+                </div>
                 <p className="film-suggested__synopsys">{film.synopsys}</p>
+                <span className="film-suggested__director"><strong>Director: </strong> {film.director}</span>
+            </div>
+            <div className="film-suggested__actions">
+                <a className="green-button" onClick={onClickNext}>
+                    Next
+                    <span>>></span>
+                </a>
             </div>
         </div>
     );
--- src/components/film/film-suggestions/film-suggestions.js
+++ src/components/film/film-suggestions/film-suggestions.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './film-suggestions.css';
 import FilmSuggestionsList from './film-suggestions-list/film-suggestions-list';
-import FilmSuggestionsResume from './film-suggestions-resume/film-suggestions-resume';
+import FilmSuggestionsResume from './film-suggestions-resume/film-suggestions-resume'; 
 
 const FilmSuggestions = ({ films, selectedFilm, onSelectSuggestedFilm }) => {
 
     const handleOnMouseOver = (selectedFilmId) => {
         onSelectSuggestedFilm(selectedFilmId);
+    }
+
+    const handleOnClickNext = () => {
+        const lastFilmIndex = films.length - 1;
+        const firstFilmIndex = 0;
+        const currentFilmIndex = films.findIndex(it => it.id === selectedFilm.id);
+        const nextFilmIndex = currentFilmIndex === lastFilmIndex ? firstFilmIndex : currentFilmIndex + 1;
+        const filmId = films[nextFilmIndex].id;
+
+        onSelectSuggestedFilm(filmId);
     }
 
     if (!selectedFilm && films.length === 0) {
@@ -22,7 +32,7 @@ const FilmSuggestions = ({ films, selectedFilm, onSelectSuggestedFilm }) => {
             <h1>More like this</h1>
             <div className="suggestions-grid">
                 <FilmSuggestionsList films={films} selectedFilm={selectedFilm} onMouseOver={handleOnMouseOver} />
-                <FilmSuggestionsResume film={selectedFilm} />
+                <FilmSuggestionsResume film={selectedFilm} onClickNext={handleOnClickNext}/>
             </div>
         </div>
     )
